2543e2d56c042ffb5e71aec5fe41b462f508ccaa
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-02-06 12:09:42 -0600

    Refactor imports and remove unused middleware

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import (
+    FastAPI,
+)
 import os
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -7,19 +9,6 @@ from db_config import create_table, data_dir
 from auth import router as auth_router
 from routers.user import router as user_router
 from routers.album import router as album_router
-
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import RedirectResponse
-
-
-class NormalizeSlashMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        path = request.url.path
-        if not path.endswith("/") and not "." in path.split("/")[-1]:
-            new_url = request.url.replace(path=path + "/")
-            return RedirectResponse(url=str(new_url), status_code=301)
-        response = await call_next(request)
-        return response
 
 
 app = FastAPI()
@@ -32,8 +21,6 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
-
-app.add_middleware(NormalizeSlashMiddleware)
 
 app.include_router(user_router)
 app.include_router(auth_router)
