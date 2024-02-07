2a8a8b14590a119fb3fff5f936bb540c41723a55
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-02-07 02:27:56 -0600

    Update router push in Home component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  router.push('/admin/albums');
+  useEffect(() => {
+    router.push('/admin/photos');
+  }, [router]);
   return (
     <div>
       <h1>PhotoAura</h1>
