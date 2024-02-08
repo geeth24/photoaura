6a7a83eae061b896d147e176b3d0aa9aa9368d01
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-02-08 15:28:16 -0600

    Refactor generateMetadata function to fetch album data from API

--- client/src/app/share/[user]/[album]/[secret]/page.tsx
+++ client/src/app/share/[user]/[album]/[secret]/page.tsx
@@ -1,28 +1,28 @@
 import SharedPage from '@/components/SharedPage';
 import { Metadata } from 'next';
 
-// export async function generateMetadata({
-//   params,
-// }: {
-//   params: { user: string; album: string, secret: string  };
-// }): Promise<Metadata> {
-//   const response = await fetch(
-//     `https://aura.reactiveshots.com/api/album/${params.user}/${params.album}/`,
-//   );
-//   const result = await response.json();
+export async function generateMetadata({
+  params,
+}: {
+  params: { user: string; album: string, secret: string  };
+}): Promise<Metadata> {
+  const response = await fetch(
+    `https://aura.reactiveshots.com/api/album/${params.user}/${params.album}/`,
+  );
+  const result = await response.json();
 
-//   return {
-//     title: `${result.album_name} | PhotoAura`,
-//     description: `View ${result.album_name} on PhotoAura`,
-//     openGraph: {
-//       images: [
-//         {
-//           url: result.album_photos[0].image,
-//         },
-//       ],
-//     },
-//   };
-// }
+  return {
+    title: `${result.album_name} | PhotoAura`,
+    description: `View ${result.album_name} on PhotoAura`,
+    openGraph: {
+      images: [
+        {
+          url: result.album_photos[0].image,
+        },
+      ],
+    },
+  };
+}
 
 function Page({ params }: { params: { user: string; album: string, secret: string } }) {
   return <SharedPage params={params} />;
