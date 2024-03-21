1fdce450abc472c4b4e947aa87943b6983c1dc47
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-03-21 17:52:38 -0500

    Update FadeInStagger margin in Page component

--- client/src/app/admin/albums/page.tsx
+++ client/src/app/admin/albums/page.tsx
@@ -215,7 +215,7 @@ function Page() {
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
-      <FadeInStagger className="mt-4 grid w-full grid-cols-1  gap-4 p-4  md:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-4 grid w-full grid-cols-1  gap-4 p-4  md:grid-cols-2 lg:grid-cols-3">
         {albums.map((album) => (
           <FadeIn key={album.album_name}>
             <Link
@@ -262,7 +262,7 @@ function Page() {
             </Link>
           </FadeIn>
         ))}
-      </FadeInStagger>
+      </div>
     </div>
   );
 }
