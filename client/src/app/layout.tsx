b88bd11ae30ac2f509d02ce387b8418d2fe89cd9
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-04-29 01:40:32 -0500

    Fix Content-Security-Policy meta tag in RootLayout component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>  */}
+      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta> 
       <link rel="icon" href="/images/logo-color.png" />
 
       <body className={lato.className}>
