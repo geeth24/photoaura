198f2a53697686fa20972445f3be85f47926cf8f
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-05-06 19:02:15 -0500

    Refactor Hero component to update background gradient colors

--- marketing/src/components/Hero.tsx
+++ marketing/src/components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
         aria-hidden="true"
       >
         <div
-          className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#7060D5] to-[#F18B62] opacity-20"
+          className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-t from-[#0582CA] to-[#00A6FB] opacity-20"
           style={{
             clipPath:
               'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
