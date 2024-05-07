b595574c345beb5940f3b57386e12b6eb491e271
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-05-07 12:51:24 -0500

    feat: logo on home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push('/admin/photos');
+    router.push('/admin/dashboard');
   }, [router]);
   return (
-    <div>
-      <h1 className="text-center text-4xl font-bold text-secondary">PhotoAura</h1>
+    <div className="flex h-screen items-center justify-center">
+      <Image src="/images/logo-blue.png" alt="PhotoAura" width={200} height={200} />
     </div>
   );
 }
